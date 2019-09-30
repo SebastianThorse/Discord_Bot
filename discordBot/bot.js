26fc4aadc893db4de1237a5e336c77f2d3982f6a
SebastianThorsen <sebbethorsen@gmail.com>
2019-09-30 10:30:41 +0200

    bot updated

--- discordBot/bot.js
+++ discordBot/bot.js
@@ -1,22 +1,71 @@
-const playersToObserv = {
+const playersToObservGame = {
     // Sembi
     "90532204471394304":(member)=>{
-    gamePlaying(getGame(member.presence.game))
+        gamePlaying(getGame(member.presence.game), member)
     },
     // Göte
     "161850513850236928" : (member) => {
-
+        gamePlaying(getGame(member.presence.game), member)
+    },
+    // Saggen
+    "307925382383009792" : (member) => {
+        gamePlaying(getGame(member.presence.game), member)
+    },
+    // Robin
+    "90532398575411200" : (member) => {
+        gamePlaying(getGame(member.presence.game), member)
+    },
+    // Deniz
+    "186898348916211712" : (member) => {
+        gamePlaying(getGame(member.presence.game), member)
+    },
+    // Jakob
+    "98571062249275392" : (member) => {
+        gamePlaying(getGame(member.presence.game), member);
     }
 };
 
-function gamePlaying(game) {
+function gamePlaying(game, member) {
     switch (game) {
         case 'Spotify':{
-            console.log("Göte");
+            if (member.user.id === '90532204471394304') {
+                member.client.channels.get('501521263882272769').send(member.user.toString() + ' Test Spotify? :upside_down: - Sembi');
+                break;
+            }
+            member.client.channels.get('501521263882272769').send(member.user.toString() + ' Test Spotify? :upside_down:');
             break;
         }
-        case 'Wow':{
-
+        case 'World of Warcraft Classic':{
+            if (member.user.id === '161850513850236928' || member.user.id === '307925382383009792') {
+                member.client.channels.get('501521263882272769').send(member.user.toString() + '  Vinner ni? :upside_down:');
+                break;
+            }
+            member.client.channels.get('501521263882272769').send(member.user.toString() + ' inte 60 ännu? :kekw:');
+            break;
+        }
+        case 'Dota 2':{
+            if (member.user.id === '161850513850236928' || member.user.id === '307925382383009792') {
+                member.client.channels.get('501521263882272769').send(member.user.toString() + ' O NI SPELER DETTA DOTA..');
+                break;
+            }
+            member.client.channels.get('501521263882272769').send(member.user.toString() + ' Dota 2 :WeirdChamp:');
+            break;
+        }
+        case 'Vermintide 2': {
+            member.client.channels.get('501521263882272769').send(member.user.toString() + ' Verminfag :DansGame:');
+            break;
+        }
+        case 'League of Legends' : {
+            if (member.user.id === '98571062249275392') {
+                member.client.channels.get('501521263882272769').send(member.user.toString() + ' the king is back! :Pog:');
+                break;
+            }
+            member.client.channels.get('501521263882272769').send(member.user.toString() + ' Leauge :LULW:');
+            break;
+        }
+        case 'Overwatch' : {
+            member.client.channels.get('501521263882272769').send(member.user.toString() + ' Overwatch! :PogU:');
+            break;
         }
     }
 }
@@ -26,9 +75,17 @@ function getGame(game) {
 }
 
 function onPresenceUpdate(oldMember, newMember) {
-    if(newMember.user.id in playersToObserv){
-       var func = playersToObserv[newMember.user.id];
-       func(newMember)
+    if(newMember.user.id in playersToObservGame){
+        let func = playersToObservGame[newMember.user.id];
+        func(newMember)
+    }
+}
+
+function onMessageGote(msg) {
+    if (msg.author.id === '90532204471394304') {
+        msg.reply(' INSERT HERE LUL')
+            .then(sent => console.log(`Sent a reply to ${sent.author.username}`))
+            .catch(console.error);
     }
 }
 
@@ -37,43 +94,19 @@ async function main() {
     const client = new Discord.Client();
     await client.login('NjI3NTY4NjYwMzc0MDkzODI0.XY-35g.B6YEI46V5xmVDgmkjJR3sMB9wjQ');
     console.log("We logged in");
-    // Used for checking userID.
-    //userId(client);
 
     client.on('ready', () => {
         console.log(`Logged in as ${client.user.tag}!`);
     });
 
-    client.on('message', msg => {
-        if (msg.content === 'ping') {
-            msg.reply('Pong!');
-        }
-    });
+    client.on('message', onMessageGote);
 
     client.on('presenceUpdate', onPresenceUpdate);
 
-
-/*    client.on('presenceUpdate', (_, newMember) => {
-        if (newMember.user.id === '90532204471394304' && newMember.presence.game && newMember.presence.game.name === 'Spotify') {
-            //console.log(newMember.presence.game.name);
-            console.log('Sembi spelar spotify');
-        }
-        if (newMember.user.id === '161850513850236928' && newMember.presence.game.name === 'World of Warcraft Classic') {
-            console.log('Göte spelar WOW')
-        }
-        if (newMember.user.id === '307925382383009792' && newMember.presence.game.name === 'World of Warcraft Classic') {
-            console.log('Saggen spelar spelar WOW')
-        }
-        if (newMember.user.id === '90532398575411200' && newMember.presence.game.name === 'World of Warcraft Classic') {
-            console.log('Robin spelar spelar WOW')
-        }
-        //console.log(member.user.id);
-        //console.log(member.presence.game.name);
-    });*/
-
 }
 
-function userId(client) {
+// Get ID from guild
+/*function userId(client) {
     client.guilds.forEach((guilds, _) => {
         guilds.members.forEach((member, _) => {
             if (member.user.username !== 'göte' && member.user.username !== "Saggen" && member.user.username !== 'Sembi' && member.user.username !== 'Mad 👻') {
@@ -82,6 +115,6 @@ function userId(client) {
             console.log(`${member.user.username} have id ${member.id}`);
         })
     });
-}
+}*/
 
 main();
